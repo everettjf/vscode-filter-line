062092cc0d3df19ceec20b56b37e8d1e29e10197
everettjf <everettjf@live.com>
2018-07-03 18:27:03 +0800

    eoml as third lib

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,7 +5,7 @@
 
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-import { EOML} from '../eoml';
+import { EOML} from 'eoml';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
