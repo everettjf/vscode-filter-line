4f22ba8b2676fa61dadd61bb91653cd61d473854
everettjf <everettjf@live.com>
2019-02-15 00:19:39 +0800

    fix

--- src/filter_base.ts
+++ src/filter_base.ts
@@ -2,7 +2,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { fdatasyncSync } from 'fs';
 
 class FilterLineBase{
 
