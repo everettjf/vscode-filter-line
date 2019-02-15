93c684e19659bb9a04a7997878d1699c5790dff6
everettjf <everettjf@live.com>
2019-02-15 23:07:09 +0800

    Update filter_base.ts

--- src/filter_base.ts
+++ src/filter_base.ts
@@ -29,8 +29,6 @@ class FilterLineBase{
     }
 
     protected filterFile(filePath: string){
-        console.log('will filter file : ' + filePath);
-
         const readline = require('readline');
         const fs = require('fs');
         var path = require('path');
@@ -50,9 +48,6 @@ class FilterLineBase{
 
             // change input path
             let newInputPath = inputPath + Math.floor(Date.now()/1000) + ext;
-            console.log('will rename');
-            console.log('from : ' + inputPath);
-            console.log('to: ' + newInputPath);
             try{
                 if(fs.existsSync(newInputPath)){
                     fs.unlinkSync(newInputPath);
