7bdbae3854489dfa573e68fbe4b062bad712900f
GOST-UA <10502791+GOST-UA@users.noreply.github.com>
2019-10-02 20:08:09 +0300

    Fix bug when menu commands were unable to open large files
    Issue: https://github.com/everettjf/vscode-filter-line/issues/12

--- src/extension.ts
+++ src/extension.ts
@@ -19,38 +19,69 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable_inputstring = vscode.commands.registerCommand('extension.filterLineByInputString', () => {
+    let disposable_filterby = vscode.commands.registerCommand('extension.filterLineBy', async (fileUri) => {
+        let path: string | undefined;
+        if (typeof fileUri !== 'undefined' && !(fileUri instanceof vscode.Uri)) {
+            console.warn('File URI validation failed');
+            return;
+        }
+        path = (fileUri) ? fileUri.fsPath : undefined;
+
+        interface Filters {
+            label: string;
+            command: string;
+        }
+
+        const filters: Array<Filters> = [
+            {label: 'Input String', command: 'extension.filterLineByInputString'},
+            {label: 'Input Regex', command: 'extension.filterLineByInputRegex'},
+            {label: 'Not Contain Input String', command: 'extension.filterLineByNotContainInputString'},
+            {label: 'Not Match Input Regex', command: 'extension.filterLineByNotMatchInputRegex'},
+            {label: 'Config File', command: 'extension.filterLineByConfigFile'}];
+
+        const choices: vscode.QuickPickItem[] = filters.map(item => Object.create({label: item.label}));
+        let choice: string | vscode.QuickPickItem | undefined = await vscode.window.showQuickPick(choices);
+        if (choice === undefined) {
+            return;
+        } else {
+            choice = choice.label;
+        }
+        await vscode.commands.executeCommand(filters.filter(val => val.label === choice)[0].command, path);
+    });
+
+    let disposable_inputstring = vscode.commands.registerCommand('extension.filterLineByInputString', (path) => {
         let filter = new FilterLineByInputString(context);
-        filter.filter();
+        filter.filter(path);
         context.subscriptions.push(filter);
     });
 
-    let disposable_inputregex = vscode.commands.registerCommand('extension.filterLineByInputRegex', () => {
+    let disposable_inputregex = vscode.commands.registerCommand('extension.filterLineByInputRegex', (path) => {
         let filter = new FilterLineByInputRegex(context);
-        filter.filter();
+        filter.filter(path);
         context.subscriptions.push(filter);
     });
 
-    let disposable_notcontaininputstring = vscode.commands.registerCommand('extension.filterLineByNotContainInputString', () => {
+    let disposable_notcontaininputstring = vscode.commands.registerCommand('extension.filterLineByNotContainInputString', (path) => {
         let filter = new FilterLineByInputString(context);
         filter.notcontain = true;
-        filter.filter();
+        filter.filter(path);
         context.subscriptions.push(filter);
     });
 
-    let disposable_notmatchinputregex = vscode.commands.registerCommand('extension.filterLineByNotMatchInputRegex', () => {
+    let disposable_notmatchinputregex = vscode.commands.registerCommand('extension.filterLineByNotMatchInputRegex', (path) => {
         let filter = new FilterLineByInputRegex(context);
         filter.notmatch = true;
-        filter.filter();
+        filter.filter(path);
         context.subscriptions.push(filter);
     });
 
-    let disposable_configfile = vscode.commands.registerCommand('extension.filterLineByConfigFile', () => {
+    let disposable_configfile = vscode.commands.registerCommand('extension.filterLineByConfigFile', (path) => {
         let filter = new FilterLineByConfigFile(context);
-        filter.filter();
+        filter.filter(path);
         context.subscriptions.push(filter);
     });
 
+    context.subscriptions.push(disposable_filterby);
     context.subscriptions.push(disposable_inputstring);
     context.subscriptions.push(disposable_inputregex);
     context.subscriptions.push(disposable_notcontaininputstring);
